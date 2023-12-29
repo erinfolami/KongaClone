7b3c4ba987b243c2fff8bcfb03f40ffe513e0969
erinemma <emmanuelerinfolami2@gmail.com>
2023-12-29 17:21:49 +0000

    Unversioned files

---
+++ app/src/main/java/com/example/kongaclone/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kongaclone.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kongaclone.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
